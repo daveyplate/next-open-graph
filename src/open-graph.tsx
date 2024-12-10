8d1ffac9934e6e3b6b275513b2990c10760b6ba6
daveycodez <david@swiftapk.com>
2024-12-10 02:29:00 -0800

    Allow null

--- src/open-graph.tsx
+++ src/open-graph.tsx
@@ -3,14 +3,14 @@ import { useRouter } from 'next/router'
 
 // Define the props interface
 interface OpenGraphProps {
-    title?: string
-    description?: string
-    image?: string
-    images?: string[]
-    ogType?: string
-    twitterCard?: string
-    url?: string
-    siteName?: string
+    title?: string | null
+    description?: string | null
+    image?: string | null
+    images?: string[] | null
+    ogType?: string | null
+    twitterCard?: string | null
+    url?: string | null
+    siteName?: string | null
 }
 
 // Update the OpenGraph component with TypeScript
@@ -38,7 +38,7 @@ export function OpenGraph({
             {/* Open Graph / Facebook */}
             {ogType && <meta key="og:type" property="og:type" content={ogType} />}
             {image && <meta key="og:image:0" property="og:image" content={image} />}
-            {images.map((img, index) => (
+            {images?.map((img, index) => (
                 <meta key={`og:image:${index}`} property="og:image" content={img} />
             ))}
             {siteName && <meta key="og:site_name" property="og:site_name" content={siteName} />}
